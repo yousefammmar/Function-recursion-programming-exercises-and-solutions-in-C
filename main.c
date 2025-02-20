7ad795e81fe8c6c7f7addaf1f8279eda44c38a26
yousef_ammar <123825108+yousefammmar@users.noreply.github.com>
2025-02-20 22:37:56 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void prim(int arr[],int st,int end){
+if(st<end){
+    printf("%d\t",arr[st]);
+    st++;
+    return prim(arr,st,end);
+}
+else return;
+}
+int main (){
+int j;
+printf("enter the size of an array: ");
+scanf("%d",&j);
+int n[j];
+for (int i=0;i<j;i++){
+    printf("enter the integer for %d:",i);
+    scanf("%d",&n[i]);
+}
+prim(n,0,j);
+return 0;
+}
